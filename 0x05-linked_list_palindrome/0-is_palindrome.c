b9133223d7b616468933581a34a59cc2dc3dadce
RodrigoSierraV <735@holbertonschool.com>
2020-05-26 23:56:38 -0500

    [Add] is palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = *head;
+
+	if (current == NULL)
+		return (1);
+	while (current)
+	{
+		current = current->next;
+	}
+	return (1);
+}
---
+++ 0x05-linked_list_palindrome/README.md
@@ -0,0 +1,5 @@
+A linked list is represented by a pointer to the first node of the linked list. The first node is called the head. If the linked list is empty, then the value of the head is NULL.
+Each node in a list consists of at least two parts:
+1) data
+2) Pointer (Or Reference) to the next node
+In C, we can represent a node using structures. Below is an example of a linked list node with integer data.
