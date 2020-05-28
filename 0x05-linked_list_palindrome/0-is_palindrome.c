0c7345a438efa3f84dc80abe624640772ec7aa69
RodrigoSierraV <735@holbertonschool.com>
2020-05-27 23:10:28 -0500

    [Add] linked list is palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -9,13 +9,27 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current = *head, *front, *rear;
+	int len = 0, half = 0, i = 0, j;
 
 	if (current == NULL)
 		return (1);
 	while (current)
 	{
 		current = current->next;
+		len++;
+	}
+	half = len / 2;
+	while (i != half)
+	{
+		front = rear = *head;
+		for (j = 0; j < i; j++)
+			front = front->next;
+		for (j = 0; j < len - (i + 1); j++)
+			rear = rear->next;
+		if (front->n != rear->n)
+			return (0);
+		i++;
 	}
 	return (1);
 }
--- 0x05-linked_list_palindrome/0-main.c
+++ 0x05-linked_list_palindrome/0-main.c
@@ -14,12 +14,12 @@ int main(void)
     head = NULL;
     add_nodeint_end(&head, 1);
     add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 88888);
     add_nodeint_end(&head, 50);
     add_nodeint_end(&head, 98);
     add_nodeint_end(&head, 98);
     add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 88888);
     add_nodeint_end(&head, 17);
     add_nodeint_end(&head, 1);
     print_listint(head);
