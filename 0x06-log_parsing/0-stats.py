e736142ae144937847cad94bfdf9234adcb0116f
RodrigoSierraV <735@holbertonschool.com>
2020-06-07 16:23:44 -0500

    [Add] log parsing

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+count = 0
+codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
+              '403': 0, '404': 0, '405': 0, '500': 0}
+file_size = 0
+for line in sys.stdin:
+    try:
+        count += 1
+        list_line = line.split()
+        code = list_line[-2]
+        line_size = int(list_line[-1])
+        codes_dict[code] += 1
+        file_size += line_size
+        if count % 10 == 0:
+            print('File size: {}'.format(file_size))
+            for k, v in codes_dict.items():
+                if v > 0:
+                    print('{}: {}'.format(k, v))
+    except KeyboardInterrupt:
+        print('File size: {}'.format(file_size))
+        for k, v in codes_dict.items():
+            if v > 0:
+                print('{}: {}'.format(k, v))
---
+++ 0x06-log_parsing/README.md
@@ -0,0 +1,8 @@
+This project is intended to do log parsing with Python:
+    ogs  are  imperative  in  the  development  and  maintenance process of many software
+    systems. They record detailed runtime  information  that  allows  developers and
+    support  engineers to monitor their systems and dissect anomalous behaviors and  errors.
+    The  increasing  scale  and  complexity  of  modern software  systems,  however,  make
+    the  volume  of  logs  explodes. In  many  cases,  the  traditional  way  of  manual  log
+    inspection becomes  impractical.  Many  recent  studies,  as  well  as  industrial tools,
+    resort to powerful text search.
