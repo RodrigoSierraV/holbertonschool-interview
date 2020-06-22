bde74efe15efd9a54c0ce99c4b550987d0b369ab
RodrigoSierraV <735@holbertonschool.com>
2020-06-22 00:02:21 -0500

    [Add] utf-8 validator

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Module to determine if a data set represents a valid UTF-8 encoding"""
+
+
+def validUTF8(data):
+    """
+    A character in UTF-8 can be 1 to 4 bytes long
+    The data set can contain multiple characters
+    The data will be represented by a list of integers
+    Return: True if data is a valid UTF-8 encoding, else return False
+    """
+    bins = list(map(lambda x: "{:08b}".format(x), data))
+    utf8 = True
+    for i in range(len(bins)):
+        if bins[i][0] == '0':
+            continue
+        if bins[i][:3] == '110' and bins[i+1][:2] == '10':
+            continue
+        elif bins[i][:4] == '1110' and bins[i+1][:2] == '10'\
+                and bins[i+2][:2] == '10':
+            continue
+        elif bins[i][:5] == '11110' and bins[i+1][:2] == '10' and\
+                bins[i+2][:2] == '10' and bins[i+3][:2] == '10':
+            continue
+        elif bins[i][:2] == '10':
+            continue
+        else:
+            utf8 = False
+
+    return utf8
